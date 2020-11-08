5237d9656dd6f8cf1649e248082e7ffb697e5a36
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-11-08 11:29:12 +0000

    change bed period

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     O-- FRONT --+
  */
 
-#define NOZZLE_TO_PROBE_OFFSET { -41, 0, -0.81 }
+#define NOZZLE_TO_PROBE_OFFSET { -41, 0, -0.88 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
